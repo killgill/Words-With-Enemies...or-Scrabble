2a64f5864d659718ede09939d46a975fbde3e2f3
Karan Singh Gill <gill@cooper.edu>
2017-11-10 12:03:55 -0800

    added tester

---
+++ tester.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public static void main(String[] args) {
+   AnagramDictionary dict = new AnagramDictionary("sowpods.txt");
+   ArrayList<String> toPrint = dict.getAnagramsOf("food");
+   for (int i =0; i<toPrint.size(); i++){
+      System.out.println(toPrint.get(i));
+   }
+}
